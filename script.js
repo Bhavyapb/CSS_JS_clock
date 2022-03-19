d7338c14a6990f4fdaa8719bbc9a19ac32b9ac22
Bhavyapb <Bhavyapchandran@gmail.com>
2022-03-19 01:15:35 -0400

    defined setDate function and created variables for each clock hand

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ const secondHand = document.querySelector(".sec-hand")
 const minHand = document.querySelector('.min-hand')
 const hourHand = document.querySelector('.hour-hand')
 
+function setDate(){
+    const now = new Date()
+    const sec = now.getSeconds()
+    const secondDegrees = ((sec/60)*360)+90
+
+    const min = now.getMinutes()
+    const minDegrees = ((min/60)*360)+90
+    
+    const hour = now.getHours()
+    const hourDegrees = ((hour/12)*360)+90
+}
