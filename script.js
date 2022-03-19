e2f05a95695a96b4f517c9232bd50b43a8450e07
Bhavyapb <Bhavyapchandran@gmail.com>
2022-03-19 01:19:18 -0400

    added transform style to hand for rotation

--- script.js
+++ script.js
@@ -6,10 +6,14 @@ function setDate(){
     const now = new Date()
     const sec = now.getSeconds()
     const secondDegrees = ((sec/60)*360)+90
+    secondHand.style.transform= `rotate(${secondDegrees}deg)`
 
     const min = now.getMinutes()
     const minDegrees = ((min/60)*360)+90
-    
+    minHand.style.transform= `rotate(${minDegrees}deg)`
+
     const hour = now.getHours()
     const hourDegrees = ((hour/12)*360)+90
+    hourHand.style.transform=`rotate(${hourDegrees}deg)`
+
 }
